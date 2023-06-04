b57b38fd3af6c0aaf8fd86ed288ce447d70fbc75
GOWTHAMDEV <gowthamtanu2000@gmail.com>
2023-06-04 13:38:29 +0530

    add calculation component and bg image

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,59 @@
 <template>
-  <div id="app">
-    <p>MGOWTHAM</p>
-  </div>
+  <el-row class="bg">
+  <img src="@/assets/wallpaperbetter.jpg" alt="">
+
+  <el-row id="app">
+    <Calculation :parentName="name">
+    </Calculation>
+  </el-row>
+</el-row>
 </template>
 
 <script>
+import Calculation from './calculation.vue'
 
 export default {
   name: 'App',
+  components: {
+    Calculation
+  },
+  data() {
+    console.log('dat')
+    return {
+      name: 'PARENT'
+
+    }
+  },
+  // computed:{
+  //   count:function(){
+  //     return this.name= 'tamil computed'
+  //   }
+  // },
+  beforeCreate() {
+    console.log('before create')
+  },
+  created() {
+    console.log('create')
+  },
+  beforeMount() {
+    console.log('mounted called')
+  },
+  mount() {
+    console.log('mounted called')
+  },
+  beforeUpdate() {
+    console.log('before update')
+  },
+  updated() {
+    console.log('update')
+  },
+  beforeDestroy() {
+    console.log('before destroy')
+  },
+  destroyed() {
+    console.log('destroy')
+  }
+
 }
 </script>
 
@@ -20,4 +66,13 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.bg img {
+
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  background-size: cover;}
 </style>
---
+++ src/calculation.vue
@@ -0,0 +1,69 @@
+<template>
+    <el-row class="totaldiv">
+        <el-card class="card">
+            <el-row>
+                <h3>MB TO GB</h3>
+            </el-row>
+        <el-form>
+            <el-col :span="11">
+                <el-form-item label="MB">
+                    <el-input v-model="mb" placeholder="MB"></el-input>
+                </el-form-item>
+            </el-col>
+            <br>
+            <br>
+            <br>
+            <el-col :span="11">
+                <el-form-item label="GB">
+                    <el-input v-model="gb" placeholder="GB"></el-input>
+                </el-form-item>
+            </el-col>
+            <el-col>
+                <el-button @click="reset">RESET</el-button>
+                <el-button @click="convert">CONVERT</el-button>
+            </el-col>
+        </el-form>
+    </el-card>
+
+    </el-row>
+</template>
+<script>
+export default {
+    name: "CalculationPage",
+    props: {
+        parentName: { type: String, default: 'tttt' }
+    },
+    data() {
+        return {
+            mb: '',
+            gb: ''
+        }
+    },
+    methods: {
+        convert() {
+            this.gb = this.mb / 1024
+            this.mb=''
+            setTimeout(console.log('5000'), 5000)
+            console.log(this)
+
+        },
+        reset() {
+            this.gb = ''
+            this.mb = ''
+        }
+    }
+}
+</script>
+<style>
+/* #secondinput {
+    margin-top: 42px;
+    margin-left: -337px;
+} */
+.totaldiv{
+    margin: auto;
+  width: 70%;
+}
+.card{
+    width:90%
+}
+</style>
